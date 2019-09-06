826cee47acb0d84e818a09d7bd41303f878fa878
Talaal Mirza <t.pineapples@gmail.com>
2019-09-05 20:33:38 -0400

    Working controls

--- src/script.js
+++ src/script.js
@@ -7,17 +7,16 @@ CANVAS.setAttribute('height', getComputedStyle(CANVAS).getPropertyValue('height'
 var WIDTH = CANVAS.width;
 var HEIGHT = CANVAS.height;
 
-var n_stars = 100;
-var speed = 1;
-var c_mindist = 150;
-var star_size = 3;
-var star_color = "#FFFFFF";
-var path_color = "#FFFFFF";
-var bg_color = "#808080";
-var request;
-
+var n_stars, speed, c_mindist, star_size, star_color, path_color, bg_color;
 var stars = [];
-CANVAS.style.backgroundColor = bg_color;
+
+n_stars = 100;
+speed = 5;
+c_mindist = 150;
+star_size = 3;
+star_color = "#FFFFFF";
+path_color = "#FFFFFF";
+bg_color = "#808080";
 
 function random_scale(n) {
   return Math.random() * n;
@@ -34,17 +33,6 @@ function e_dist(p1_x, p1_y, p2_x, p2_y) {
   );
 }
 
-for (var i = 0; i < n_stars; i++) {
-  var s = {
-    x: random_scale(WIDTH - star_size) + star_size,
-    y: random_scale(HEIGHT - star_size) + star_size,
-    v_x: random_real(speed),
-    v_y: random_real(speed),
-    color: star_color
-  };
-  stars.push(s);
-}
-
 function draw_stars() {
 
   stars.forEach(s => {
@@ -54,7 +42,7 @@ function draw_stars() {
     CTX.fill();
 
     stars.forEach(ss => {
-      d = e_dist(s.x, s.y, ss.x, ss.y)
+      d = e_dist(s.x, s.y, ss.x, ss.y);
       if (d <= c_mindist && d > 0) {
         CTX.moveTo(s.x, s.y);
         CTX.strokeStyle = CTX.fillStyle = path_color;
@@ -69,7 +57,12 @@ function draw_stars() {
 
 
 function animate() {
+  //CLEAR Canvas
   CTX.canvas.width = CTX.canvas.width;
+  CTX.fillStyle = bg_color;
+  CTX.fillRect(0,0, WIDTH, HEIGHT);
+
+  //UPDATE star values
   stars.forEach(s => {
     if (s.x + s.v_x <= star_size || s.x + s.v_x >= WIDTH - star_size)
       s.v_x *= -1;
@@ -77,10 +70,11 @@ function animate() {
       s.v_y *= -1;
     s.x += s.v_x;
     s.y += s.v_y;
+    s.color = star_color;
   });
 
+  //DRAW Stars
   draw_stars();
-
   window.requestAnimationFrame(animate);
 }
 
@@ -96,21 +90,68 @@ window.wallpaperPropertyListener = {
         return Math.ceil(c * 255);
       }) + ')';
     }
+    if (properties.linecolor) {
+      path_color = 'rgb(' + properties.linecolor.value.split(' ').map(function (c) {
+        return Math.ceil(c * 255);
+      }) + ')';
+    }
     if (properties.nstars) {
       n_stars = properties.nstars.value;
+      // if(n_stars < stars.length){
+      //   while(stars.length != n_stars){
+      //     stars.pop();
+      //   }
+      // } else if(n_stars > stars.length){
+      //   while(n_stars != stars.length){
+      //     n_stars.push({
+      //       x: random_scale(WIDTH - star_size) + star_size,
+      //       y: random_scale(HEIGHT - star_size) + star_size,
+      //       v_x: random_real(speed),
+      //       v_y: random_real(speed),
+      //       color: star_color
+      //     });
+      //   }
+      // }
+      stars = [];
+      create_stars(n_stars);
+      clear_canvas();
     }
     if (properties.starsize) {
       star_size = properties.starsize.value;
     }
-    if (properties.speed) {
-      speed = properties.speed.value;
+    if (properties.starspeed) {
+      speed = properties.starspeed.value;
+      stars.forEach(s => {
+        s.v_x = random_real(speed);
+        s.v_y = random_real(speed);
+      });
     }
     if (properties.linelength) {
-      c_mindist = properties.c_mindist.value;
+      c_mindist = properties.linelength.value;
     }
   }
 };
 
+function create_stars(n){
+  for (var i = 0; i < n; i++) {
+    var s = {
+      x: random_scale(WIDTH - star_size) + star_size,
+      y: random_scale(HEIGHT - star_size) + star_size,
+      v_x: random_real(speed),
+      v_y: random_real(speed),
+      color: star_color
+    };
+    stars.push(s);
+  }
+}
+
+function clear_canvas(){
+  CTX.canvas.width = CTX.canvas.width;
+  CTX.fillStyle = bg_color;
+  CTX.fillRect(0,0, WIDTH, HEIGHT);
+}
+
 function load(){
+  create_stars(n_stars);
   animate();
 }
